5f70621d4937226b3f4512e1c4f91e3cca4b5e75
gillichu <gillichu@berkeley.edu>
2023-04-17 21:41:46 -0400

    Notes mapping to ncflow og github.

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,8 +1,18 @@
+import networkx as nx
 
+# https://github.com/netcontract/ncflow/blob/9879bb1c36637acea2692c81c628f6c81a375d7c/lib/problem.py#L337
 def read_graphml():
+	# input: graphml file
+	# output: nx.MultiDiGraph
 	pass 
 
 def read_traffic_matrix(): 
+	# input: traffic matrix pkl
+	# output: dictionary
 	pass 
 
+def generate_traffic_matrix():
+	pass
+
+# find the largest strongly connected components
 
