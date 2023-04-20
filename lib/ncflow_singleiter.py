e83a44cb2665bdd60a95f1f1efa2f601d766cf27
gillichu <gillichu@berkeley.edu>
2023-04-20 16:34:57 -0400

    New code for r1_lp using the new functions in path.py. Unfortunately, graph input is still weird since path_meta is throwing 'no path found in path_meta_pair!' warning.

--- lib/ncflow_singleiter.py
+++ lib/ncflow_singleiter.py
@@ -17,15 +17,15 @@ def solve_lp():
 
 def r1_lp(paths_dict, agg_commodities_dict):
     ### expects input:
-    ### paths_dict[(u_meta, v_meta)] = [([e1, e2, e3, ..., eN], mincap), ...]
-    
+    ### paths_dict[(u_meta, v_meta)] = [([e1, e2, e3, ..., eN], mincap)]
+
     r1_outfile = 'r1_out.txt'
     commodities = []
     r1_path_to_commodities = dict()
     r1_commodity_to_pathids = defaultdict(list)
+    edge_to_paths = defaultdict(list)
     
     path_idx = 0
-    path_to_pathidx = dict()
 
     for key in agg_commodities_dict.keys():
         commodity_k, pathinfo = key
@@ -33,10 +33,10 @@ def r1_lp(paths_dict, agg_commodities_dict):
 
         # get single path between each pair of meta nodes
         paths = paths_dict[(s_k, t_k)]
-        assert len(paths) == 1
         
         path = paths[0]
-        path_to_pathidx[path] = path_idx
+        for edge in path_to_edge_list(path):
+            edge_to_paths[edge].append(path_idx)
 
         path_ids = []
         path_ids.append(path_idx)
@@ -63,9 +63,13 @@ def r1_lp(paths_dict, agg_commodities_dict):
         m.addConstr(quicksum(path_variables[p] for p in path_ids) <= d_k)
 
     # add edge capacity constraints 
-    for u, v in paths_dict:
-        path, c_e = paths_dict[(u, v)]
-        constr_vars = [path_variables[path_to_pathidx[p]] for p in paths]
+    for u, v in paths_dict: # for each intercluster edge in the selected paths dictionary
+        # paths_dict[(u_meta, v_meta)] = [([e1, e2, e3, ..., eN], mincap)]
+        _, c_e = paths_dict[(u, v)]
+        path_indices = edge_to_paths[(u, v)] 
+
+        # ensure that all paths on a given edge meet the edge constraint
+        constr_vars = [path_variables[p] for p in path_indices]
         m.addConstr(quicksum(constr_vars) <= c_e)
 
     return LpSolver(m, None, r1_outfile), r1_path_to_commodities
@@ -101,15 +105,16 @@ if __name__ == '__main__':
     G = toy_network_2()
     tm = generate_uniform_tm(G)
     num_clusters = int(np.sqrt(len(G.nodes)))
+    iter_id = 0
 
     G_agg, agg_edge_dict, agg_to_orig_nodes, orig_to_agg_node, G_clusters_dict, agg_commodities_dict,clusters_commodities_dict = construct_subproblems(G, tm, num_clusters=num_clusters)
 
     # original edges on the collapsed graph, outputs dict (metanode, metanode) = set of paths
-    paths_dict = path_meta(G, num_clusters, agg_edge_dict, agg_to_orig_nodes)
+    paths_dict = path_meta(G, G_agg, num_clusters, agg_edge_dict, agg_to_orig_nodes, iter_id)
 
     # select paths for r1, this iteration
-    iter_id = 0
     selected_inter_edges = select_inter_edge(G, agg_edge_dict, iter_id)
+    print('selected_inter_edges', selected_inter_edges)
     
     r1_solver, r1_path_to_commod = r1_lp(selected_inter_edges, agg_commodities_dict)
     print(r1_solver.solve_lp(Method.BARRIER))
