49c55de057bf3ea0c142b27492981c008d2938ce
simran135 <pcs@simran-kaur.com>
2023-04-23 16:30:07 -0400

    added first version of reconciliation (still need to test using r2 output)

--- lib/ncflow_singleiter.py
+++ lib/ncflow_singleiter.py
@@ -137,7 +137,92 @@ def reconciliation_lp():
     pass
 
 def r3_lp():
-    pass
+    reconciliation_outfile = 'reconciliation_out.txt'
+    os.remove(reconciliation_outfile)
+
+    # create a subgraph to represent flows from u_meta to v_meta
+    nodes_in_u_meta = set()
+    nodes_in_v_meta = set()
+    G_u_meta_v_meta = nx.DiGraph()
+
+    v_hat_ins, v_hat_outs = v_hat_dict(G_agg)
+    u_hat_in, v_hat_out = v_hat_ins[u_meta], v_hat_outs[v_meta]
+
+
+    all_u_meta_v_meta_inter_edges = [(u,v,G[u][v][CAPACITY]) for u in agg_to_orig_nodes[u_meta] \
+                                     for v in G.successors(u) if orig_to_agg_node[v]==v_meta]
+    interedge_to_idx = {(u,v): idx for idx, (u, v, _) in enumerate(all_u_meta_v_meta_inter_edges)}
+
+    for (u, v, capacity) in all_u_meta_v_meta_inter_edges:
+        if u not in nodes_in_u_meta:
+            nodes_in_u_meta.add(u)
+            G_u_meta_v_meta.add_node(u)
+        if v not in nodes_in_v_meta(v):
+            nodes_in_v_meta.add(v)
+            G_u_meta_v_meta.add_node(v)
+        G_u_meta_v_meta.add_edge(u, v, capacity)
+
+    r2_u_meta_sol_dict, r2_v_meta_sol_dict = r2_solution_dict[u_meta], r2_solution_dict[v_meta]
+
+    # only use meta commodity solution from R2 for flows from u_meta to v_meta
+    shared_meta_commodities = set(r2_u_meta_sol_dict.keys()).intersection(set(r2_v_meta_sol_dict.keys()))
+    shared_meta_commodities = [commod for commod in shared_meta_commodities if sum([v==v_hat_out for (u,v), _ in r2_u_meta_sol_dict[commod]]) > 0]
+    shared_meta_commodities = [commod for commod in shared_meta_commodities if sum([u==u_hat_in for (u,v), _ in r2_v_meta_sol_dict[commod]]) > 0]
+
+    m = Model(f"reconciliation [meta_node (out) = {u_meta}][meta_node (in) = {v_meta}]")
+
+    num_edges, num_commodities = len(all_u_meta_v_meta_inter_edges), len(shared_meta_commodities)
+    commodity_vars = m.addVars(num_edges, num_commodities, vtype=GRB.CONTINUOUS, lb=0.0, name='f')
+
+    # edge capacity constraints
+    for edge_idx, (u, v, capacity) in enumerate(all_u_meta_v_meta_inter_edges):
+        print(f"Adding capacity constraint: edge {edge_idx} <= {capacity}")
+        m.addConstr(commodity_vars.sum(edge_idx, '*') <= capacity, f"capacity [edge_idx_{edge_idx}][u {u}][v {v}]")
+
+
+    # flow constraints
+    total_u_meta_flows, total_v_meta_flows = dict(), dict()
+    for k_local, shared_meta_commodity in enumerate(shared_meta_commodities):
+        u_meta_flows, v_meta_flows = r2_u_meta_sol_dict[shared_meta_commodity], r2_v_meta_sol_dict[shared_meta_commodity]
+
+        k_meta, (s_k, t_k, d_k) = shared_meta_commodity
+
+        # sum outflow from u <= f_{k_local}
+        u_outflows_dict = defaultdict()
+        for (u,v), outflow in u_meta_flows:
+            if v==v_hat_out:
+                u_outflows_dict[u] += outflow
+        u_outflows_dict = {u: outflow if outflow > 0.0 else 0.0 for u, outflow in u_outflows_dict.items()}
+
+        for u in nodes_in_u_meta:
+            outflow = u_outflows_dict[u]
+            total_u_meta_flows[k_meta] += outflow
+
+            outgoing_inter_edges_to_idx = [interedge_to_idx[(u,v)] for v in G_u_meta_v_meta.successors(u)]
+            print(f"Adding outflow constraint: (sum of commods for edges {outgoing_inter_edges_to_idx}) <= {outflow}")
+            m.addConstr(quicksum(commodity_vars[edge, k_local] for edge in outgoing_inter_edges_to_idx) <= outflow, f"outflow [u {u}]][k_meta {k_meta}]")
+
+        # sum of inflow to v <= f_{k_local}
+        v_inflows_dict = defaultdict()
+        for (u,v), inflow in v_meta_flows:
+            if u==u_hat_in: 
+                v_inflows_dict[v] += inflow
+        v_inflows_dict = {v: inflow if inflow > 0.0 else 0.0 for v, inflow in v_inflows_dict.items()}
+
+        for v in nodes_in_v_meta:
+            inflow = v_inflows_dict[v]
+            total_v_meta_flows[k_meta] += inflow
+
+            incoming_inter_edges_to_idx = [interedge_to_idx[(u,v)] for u in G_u_meta_v_meta.predecessors(v)]
+            print(f"Adding inflow constraint: (sum of commods for edges {incoming_inter_edges_to_idx}) <= {inflow}")
+            m.addConstr(quicksum(commodity_vars[edge, k_local] for edge in incoming_inter_edges_to_idx) <= inflow, f"inflow [v {v}]][k_meta {k_meta}]")
+
+    # objective is to maximize flow
+    obj = quicksum(commodity_vars)
+    m.setObjective(obj, GRB.MAXIMIZE)
+
+    return LpSolver(m, None, reconciliation_outfile), G_u_meta_v_meta, shared_meta_commodities, nodes_in_u_meta, nodes_in_v_meta
+
 
 if __name__ == '__main__':
     #G = toy_network_1()
